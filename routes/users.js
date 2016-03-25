8b06e57b7c085d23f4fcdf9d8311536e466576c4
Ville Seppälä <ville@villeseppala.com>
2016-03-25 22:59:28 +0200

    Change splits routes to separate files

---
+++ routes/users.js
@@ -0,0 +1,7 @@
+import route from 'koa-route';
+import * as users from '../controller/users';
+
+export default (app) => {
+    app.use(route.get('/api/users', users.all));
+    app.use(route.get('/api/user/:id', users.get));
+};
